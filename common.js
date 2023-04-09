87b0cd5fbe3b4f059c508b1960d1d5bf084ffe1d
Adrian-Lorente <100451137@alumnos.uc3m.es>
2023-04-09 17:08:14 +0200

    Nav fijado y linkeado a las secciones. Un poco de párrafo

---
+++ common.js
@@ -0,0 +1,6 @@
+let currentPageNav = document.getElementById("current");
+if (currentPageNav != null ||currentPageNav != undefined){
+    currentPageNav.style.backgroundColor = "#B5F1CC";
+    currentPageNav.style.borderRadius = "10px 20px";
+    currentPageNav.style.color = "000"
+}
