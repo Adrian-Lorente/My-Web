e33b136c0e98995ae581d462a5bea0e12810ed37
Adrian-Lorente <100451137@alumnos.uc3m.es>
2023-11-09 23:38:44 +0100

    Another try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr:false, 
     css: [
       'bootstrap/dist/css/bootstrap.min.css',
       '@/assets/common.css'
